51c59429f786b6ce37f078080ed869ad0bc0097e
Josh Bohde <josh.bohde@gmail.com>
2012-01-25 14:41:10 -0600

    Probably broken link scraping of pages.

--- src/etta/core.clj
+++ src/etta/core.clj
@@ -1,4 +1,5 @@
 (ns etta.core
+  (:require clojure.string)
   (:require [net.cgrand.enlive-html :as html]))
 
 (defn fetch-url [url]
@@ -7,6 +8,32 @@
 (defn links [doc]
   (map (comp :href :attrs) (html/select doc [:a])))
 
-;(links (fetch-url "http://google.com/"))
+(defn base-url [doc]
+  (first (map (comp :href :attrs) (html/select doc [:base]))))
 
+(def domain-re #"^\w+\://(\w|\.)+/?")
+
+(defn has-domain? [url]
+  (boolean (seq (re-seq domain-re url))))
+
+(defn get-domain [url]
+  (first (first (re-seq domain-re url))))
+
+(defn is-rel? [url]
+  (not (seq (re-seq #"^/" url))))
+
+(defn force-directory [path]
+  (clojure.string/replace (str "/" path "/") #"/+" "/"))
+
+(defn normalize-link [domain base link]
+  (cond
+   (has-domain? link) link
+   (is-rel? link) (str domain (force-directory base) link)
+   :else (str domain link)))
+
+(defn links-for-url [url doc]
+  (let [domain (get-domain url)
+        doc-base (base-url doc)
+        base (if doc-base doc-base "/")]
+    (map (partial normalize-link domain base) (links doc))))
 
